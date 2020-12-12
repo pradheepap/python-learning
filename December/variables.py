dc6ec6ef6d415280f7ef244011b0cc73d5afed7d
pradheepa <pradheepa.p@gmail.com>
2020-12-12 21:12:55 +0800

    Assign same value to multiple variables in a single line

--- December/variables.py
+++ December/variables.py
@@ -34,9 +34,18 @@ print(type(helloString))
 
 '''
 Many Values to Multiple Variables
-
+Make sure the number of variables matches the number of values, or else you will get an error.
 '''
 x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+
+'''
+One Value to Multiple Variables
+'''
+x = y = z = "Orange"
 print(x)
 print(y)
 print(z)
