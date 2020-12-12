ab3df3ab47667e3dd0a98f98454cbff456ee4125
pradheepa <pradheepa.p@gmail.com>
2020-12-12 20:59:24 +0800

    Variables in Python

---
+++ December/variables.py
@@ -0,0 +1,25 @@
+## No need to declare the variables. They
+### are created when the value is assigned
+
+number = 4
+helloString = "Hello Hi!!!"
+
+print (number)
+print (helloString)
+ 
+
+ # Explicitly assign the type of the variable
+
+number = int(6)
+helloString = str ("Its me")
+
+print (number)
+print (helloString)
+
+## Changing integer to String
+"""
+This is a multiline comment
+The integer variable is modified to String and printed.
+"""
+number = str("Integer modified to String")
+print (number)
